6b86aa53076338d00f7700221b099b0f53d027e7
Nikita <anisimoff.nikita@gmail.com>
2017-10-01 13:32:39 +0300

    image loading

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,6 +1,9 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <QFileDialog>
+#include <QPixmap>
+
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
@@ -11,4 +14,13 @@ MainWindow::MainWindow(QWidget *parent) :
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_actOpenImage_triggered()
+{
+    QString filename = QFileDialog::getOpenFileName(this,
+                                                    "Open Image",
+                                                    "~",
+                                                    "Image Files (*.png *.jpg *.bmp)");
+    ui->lblImage->setPixmap(QPixmap(filename));
 }
